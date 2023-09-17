bd7bb6760bed7e4eba8a3225aa2966ad09d832a7
Luciano Ramalho <luciano@ramalho.org>
2023-09-17 20:44:08 -0300

    reorganização e refatoração de exemplos

--- concorrencia/primes/1_primo.py
+++ concorrencia/primes/1_primo.py
@@ -2,11 +2,13 @@
 
 from primes import is_prime
 
+
 def main():
     n = 7_777_777_777_777_753
     primo = is_prime(n)
     e_nao_e = 'é' if primo else 'não é'
-    print(n, e_nao_e, 'primo' )
+    print(n, e_nao_e, 'primo')
+
 
 if __name__ == '__main__':
     main()
--- concorrencia/primes/1_primo_proc.py
+++ concorrencia/primes/1_primo_proc.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 import itertools
-from multiprocessing import Process, Event
+from multiprocessing import Process
+from multiprocessing.synchronize import Event
 
 from primes import is_prime
+
 
 def girar(msg: str, pronto: Event) -> None:
     for char in itertools.cycle(r'\|/-'):
         status = f'\r{char} {msg}'
         print(status, end='', flush=True)
-        if pronto.wait(.05):
+        if pronto.wait(0.05):
             break
     blanks = ' ' * len(status)
     print(f'\r{blanks}\r', end='')
@@ -24,8 +26,7 @@ def main():
     pronto.set()
     girador.join()
     e_nao_e = 'é' if primo else 'não é'
-    print(n, e_nao_e, 'primo' )
-
+    print(n, e_nao_e, 'primo')
 
 
 if __name__ == '__main__':
