a83d9666e6ac800f6cce4773f3b5a162b87c072d
Luciano Ramalho <luciano@ramalho.org>
2023-10-03 17:35:44 -0300

    exemplos com LPF (least prime factor)

--- concorrencia/primes/integers.py
+++ concorrencia/primes/integers.py
@@ -10,6 +10,6 @@ assert MAX_UINT64 == 2**64 - 1
 
 print(f'{MAX_UINT64:26d}  // max uint64')
 print()
-for i in range(42, 65, 2):
+for i in range(50, 65, 2):
     n = 2**i
     print(f'{n:26d},  // 2 ** {i}')
